734334304c18be1dc4ae5f75cec508a9e923f933
juraj13 <obradovic.juraj@gmail.com>
2020-09-20 23:18:10 +0200

    tf frames positions found

--- scripts/virtual_wall.py
+++ scripts/virtual_wall.py
@@ -4,6 +4,7 @@ import rospy
 import tf
 from nav_msgs.msg import OccupancyGrid
 from nav_msgs.msg import Odometry
+from tf import TransformListener
 
 
 class VirtalWall(object):
@@ -33,10 +34,19 @@ class VirtalWall(object):
 		self.robot_pix_x, self.robot_pix_y = self.position_to_pixels(self.robot_x, self.robot_y)
 
 	def position_to_pixels(self, pose_x, pose_y):
+
 		pix_x = int((pose_x - self.map_origin.x) / self.map_resolution)
 		pix_y = int((pose_y - self.map_origin.y) / self.map_resolution)
 		return pix_x, pix_y
 
+	def get_tf_transform(self, tf1, tf2):
+
+		self.listener.waitForTransform(tf1, tf2, rospy.Time(), rospy.Duration(4.0))
+		(trans,rot) = self.listener.lookupTransform(tf1, tf2, rospy.Time(0))
+		print 'transformation between tf1 and tf2 detected'
+		print 'translation vector: (',trans[0],',',trans[1],',',trans[2],')'
+		print 'rotation : x=',rot[0],' y=',rot[1],' z=',rot[2], 'w=',rot[3]
+		return trans, rot
 
 	def __init__(self):
 
@@ -44,6 +54,9 @@ class VirtalWall(object):
 		rospy.sleep(0.5) 
 		rospy.Subscriber("/map", OccupancyGrid, self.map_callback, queue_size = 1)
 		rospy.sleep(0.5) 
+		self.listener = tf.TransformListener()
+		trans_p1, rot_p1 = self.get_tf_transform('/ego_racecar/p1', '/ego_racecar/base_link')
+		trans_p2, rot_p2 = self.get_tf_transform('/ego_racecar/p2', '/ego_racecar/base_link')
 		rospy.spin()
 
 
