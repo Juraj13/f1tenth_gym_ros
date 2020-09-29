96b9cfa6d826bcbc19a85330396966685c17e2e4
Ivan Pavlak <ivan.pavlak3@gmail.com>
2020-09-30 00:10:10 +0200

    global trajectory planner implemented, trajectory written to csv file

--- scripts/TrajectoryPlanner.py
+++ scripts/TrajectoryPlanner.py
@@ -4,12 +4,14 @@ from math import sin, cos, tan, atan2, sqrt, pi
 
 
 class TrajectoryPlanner:
-    def __init__(self, trajectory_points, theta_0, N):
+    def __init__(self, trajectory_points, theta_0, N, epsilon=1e-5):
         self.trajectory_points = trajectory_points
         self.trajectory = np.empty((0,2))
         self.theta0 = theta_0
         self.thetas = np.array([theta_0])
-        self.N = N          # number of interpolated points
+        self.N = N              # number of interpolated points
+        self.epsilon = epsilon  # when are 2 values equal
+        self.Inf = 1e9          # infinity
 
 
     def add_trajectory_point(self, pos):
@@ -56,7 +58,7 @@ class TrajectoryPlanner:
         #     return self.minimum_curvature(pos_0, orientation_0, pos_f)
         # elif method == 'constcurv':
         #     return self.constant_curvature(pos_0, orientation_0, pos_f)
-        return self.constant_curvature(pos_0, orientation_0, pos_f)
+        return self.minimum_curvature(pos_0, orientation_0, pos_f)
         if self.relative_curvature(pos_0, orientation_0, pos_f) < 0.7:
             print("minimum curvature")
             return self.minimum_curvature(pos_0, orientation_0, pos_f)
@@ -69,63 +71,29 @@ class TrajectoryPlanner:
         "uses Newton-Raphson method for optimization"
         x0 = pos_0[0]; y0 = pos_0[1]; theta0 = orientation_0
         xf = pos_f[0]; yf = pos_f[1]
-
-        ka = xf - x0
-        kb = yf - y0
-
-        tg0 = tan(theta0)
         
-        if abs(tg0) > 1:
-            ctg0 = 1 / tg0
-            A = np.array( [[1, 3, 0, 0, 0],
-                           [0, 0, 0, 1, 3],
-                           [ctg0, 0, 0, 1, 0],
-                           [1, 1, ctg0, 0, 0],
-                           [0, 0, 1, 1, 1]] )
-        else:
-            A = np.array( [[0, 1, 3, 0, 0],
-                           [0, 0, 0, 1, 3],
-                           [0, 1, 0, tg0, 0],
-                           [1, 1, 1, 0, 0],
-                           [tg0, 0, 0, 1, 1]] )
+        A = np.array( [[ 2, 6, 12, 20, 0, 0, 0, 0, 1, 0],
+                       [ 6, 18, 24, 60, 0, 0, 0, 0, 1, 0],
+                       [ 12, 24, 48, 120, 0, 0, 0, 0, 1, 0],
+                       [ 20, 60, 120, 200, 0, 0, 0, 0, 1, 0],
+                       [ 0, 0, 0, 0, 2, 6, 12, 20, 0, 1],
+                       [ 0, 0, 0, 0, 6, 18, 24, 60, 0, 1],
+                       [ 0, 0, 0, 0, 12, 24, 48, 120, 0, 1],
+                       [ 0, 0, 0, 0, 20, 60, 120, 200, 0, 1],
+                       [ 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+                       [ 0, 0, 0, 0, 1, 1, 1, 1, 0, 0]] )
 
         inv_A = np.linalg.inv(A)
-        b = np.array([0, 0, 0, ka, kb]).reshape((5,1))
-        spline_coeff = np.matmul(inv_A, b)
 
-        if abs(tg0) > 1:
-            a0 = x0; a2 = spline_coeff[0]; a3 = spline_coeff[1]
-            b0 = y0; b1 = spline_coeff[2]; b2 = spline_coeff[3]; b3 = spline_coeff[4]
-            a1 = b1*ctg0
-        else:
-            a0 = x0; a1 = spline_coeff[0]; a2 = spline_coeff[1]; a3 = spline_coeff[2]
-            b0 = y0; b1 = a1*tg0; b2 = spline_coeff[3]; b3 = spline_coeff[4]
-
-        # wrong angle (tangens -> 2 possible solutions)
-        if abs(atan2(b1, a1) - theta0) > 1e-3:
-            print('fulo skroz kut')
-            A = [ [1, 3, 0, 0],
-                  [0, 0, 1, 3],
-                  [1, 1, 0, 0],
-                  [0, 0, 1, 1] ]
-            inv_A = np.linalg.inv(A)
-            b = np.array([0, 0, ka+a1, kb+b1]).reshape((4,1))
-            spline_coeff = np.matmul(inv_A, b)
-
-            if abs(tg0) > 1:
-                a0 = x0; a2 = spline_coeff[0]; a3 = spline_coeff[1]
-                b0 = y0; b1 = -b1; b2 = spline_coeff[2]; b3 = spline_coeff[3]
-                a1 = b1*ctg0
-            else:
-                a0 = x0; a1 = -a1; a2 = spline_coeff[0]; a3 = spline_coeff[1]
-                b0 = y0; b1 = a1*tg0; b2 = spline_coeff[2]; b3 = spline_coeff[3]
-        
+        [a, b, K] = self.bestK(x0, y0, xf, yf, theta0, inv_A, 100)
+        # print(K)
 
         u = np.linspace(0, 1, self.N)
-        x = a3*u**3 + a2*u**2 + a1*u + a0
-        y = b3*u**3 + b2*u**2 + b1*u + b0
+        x = a[5]*u**5 + a[4]*u**4 + a[3]*u**3 + a[2]*u**2 + a[1]*u + a[0]
+        y = b[5]*u**5 + b[4]*u**4 + b[3]*u**3 + b[2]*u**2 + b[1]*u + b[0]
 
-        thetaf = atan2(3*b3 + 2*b2 + b1, 3*a3 + 2*a2 + a1)
+        thetaf = atan2(5*b[5] + 4*b[4] + 3*b[3] + 2*b[2] + b[1],  \
+                       5*a[5] + 4*a[4] + 3*a[3] + 2*a[2] + a[1])
         # print(pos_0, theta0)
         return x, y, thetaf
 
@@ -138,12 +106,12 @@ class TrajectoryPlanner:
         tg0 = tan(theta0)
 
         # special cases
-        if abs(atan2(yf-y0, xf-x0) - theta0) < 1e-5:
+        if abs(atan2(yf-y0, xf-x0) - theta0) < self.epsilon:
             print('colinear points')
             x = np.linspace(x0, xf, self.N)
             y = np.linspace(y0, yf, self.N)
             return x, y, theta0
-        elif abs(atan2(yf-y0, xf-x0) + theta0) < 1e-5:
+        elif abs(atan2(yf-y0, xf-x0) + theta0) < self.epsilon:
             print("sjebo si nes")
             return
         
@@ -157,7 +125,7 @@ class TrajectoryPlanner:
             print("x0 == xf")
             cy = (y0+yf) / 2
             cx = x0 - tg0*(cy-y0)
-            if abs( (cx-x0) / (cy-y0) + tg0) > 1e-5: # maybe unneccessary
+            if abs( (cx-x0) / (cy-y0) + tg0) > self.epsilon: # maybe unneccessary
                 print("wrong angle")
         else:
             k1 = (x0**2 + y0**2 - xf**2 - yf**2) / (2*(x0-xf))
@@ -166,7 +134,7 @@ class TrajectoryPlanner:
             cx = k1 - k2*cy
         
             # wrong angle
-            if abs( (cx-x0) / (cy-y0) + tg0) > 1e-5:
+            if abs( (cx-x0) / (cy-y0) + tg0) > self.epsilon:
                 print("wrong angle %f %f" %( (cx-x0) / (cy-y0), tg0))
                 cy = (y0*tg0 + k1 - x0) / (k2 + tg0)
                 cx = k1 - k2*cy
@@ -200,6 +168,52 @@ class TrajectoryPlanner:
         y = cy + R*np.sin(u)
         
         return x, y, self.angle_domain(thetaf)
+
+
+    def bestK(self, x0, y0, xf, yf, theta0, inv_A, N):
+        u = np.linspace(0, 1, N)
+        delta_theta = np.zeros(N-1)
+        ks = np.array([i for i in range(30, 200)])
+        curveInts = np.zeros(len(ks))
+
+        a0 = x0; b0 = y0
+        for j in range(len(ks)):
+            k = ks[j]
+            a1 = k*cos(theta0)
+            b1 = k*sin(theta0)
+            ka = xf - a0 - a1; kb = yf - b0 - b1
+            b = np.array([0, 0, 0, 0, 0, 0, 0, 0, ka, kb]).transpose()
+            spline_coeff = np.matmul(inv_A, b)
+            a = np.array([a0, a1, spline_coeff[0], spline_coeff[1], spline_coeff[2], spline_coeff[3]])
+            b = np.array([b0, b1, spline_coeff[4], spline_coeff[5], spline_coeff[6], spline_coeff[7]])
+            x = a[5]*u**5 + a[4]*u**4 + a[3]*u**3 + a[2]*u**2 + a[1]*u + a[0]
+            y = b[5]*u**5 + b[4]*u**4 + b[3]*u**3 + b[2]*u**2 + b[1]*u + b[0]
+
+            delta_theta[0] = self.angdiff( atan2(y[1]-y[0], x[1]-x[0]), theta0)**2
+            if delta_theta[0] > 1.0:
+                delta_theta[0] = self.Inf
+            for i in range(1, len(delta_theta)-1):
+                delta_theta[i] = self.angdiff( atan2(y[i]-y[i-1], x[i]-x[i-1]), atan2(y[i+1]-y[i], x[i+1]-x[i]) )**2
+                if delta_theta[i] > 1.0:
+                    delta_theta[i] = self.Inf
+            curveInts[j] = np.trapz(delta_theta)
+        
+        M = np.min(curveInts)
+        I = np.argmin(curveInts)
+        K = ks[I]
+        a1 = K*cos(theta0); b1 = K*sin(theta0)
+        ka = xf - a0 - a1; kb = yf - b0 - b1
+        b = np.array([0, 0, 0, 0, 0, 0, 0, 0, ka, kb]).transpose()
+        spline_coeff = np.matmul(inv_A, b)
+        a = np.array([a0, a1, spline_coeff[0], spline_coeff[1], spline_coeff[2], spline_coeff[3]])
+        b = np.array([b0, b1, spline_coeff[4], spline_coeff[5], spline_coeff[6], spline_coeff[7]])
+        return [a, b, K]
+
+
+    def angdiff(self, th1, th2):
+        d = th1 - th2
+        d = np.mod(d+np.pi, 2*np.pi) - np.pi
+        return d
 
     def relative_curvature(self, pos, theta, newPos):
         v1 = [cos(theta), sin(theta)]
