747993b1c782000a39e539d65d631aa97310f147
Ivan Pavlak <ivan.pavlak3@gmail.com>
2020-10-16 23:28:33 +0200

    Experiment, more heuristically

--- scripts/speed_vs_curvature_systemID.py
+++ scripts/speed_vs_curvature_systemID.py
@@ -9,13 +9,13 @@ from f1tenth_gym_ros.msg import RaceInfo
 # car parameters
 car_width = 0.31
 car_length = 0.58
+car_length = 0.32
 
 class SpeedVsCurvatureExperiment(object):
     "Drive half circles and visualize paths"
 
-    def __init__(self, curvature_radius, robot_velocity):
+    def __init__(self, driving_angle, robot_velocity, curvature_radius=0):
         # class variables
-        self.curv_R = curvature_radius
         self.robot_vel = robot_velocity
         self.velocity_reached = False
         self.paths_published = False
@@ -27,9 +27,15 @@ class SpeedVsCurvatureExperiment(object):
         self.robot_path = Path()
         self.cx = 0     # circle center
         self.cy = 0     # circle center
-        self.angle = -atan2(car_length, self.curv_R)  # driving angle (ackermann) - direction
-        self.angle = -0.0321
+        self.angle = -driving_angle     # turning angle is absolute number
+        if curvature_radius == 0:
+            self.curv_R = car_length / tan(-self.angle)
+        else:
+            self.curv_R = curvature_radius
+        # self.angle = -atan2(car_length, self.curv_R)  # driving angle (ackermann) - direction
+        # self.angle = -0.0321
         print("Driving angle = %f" % self.angle)
+        print("Curvature radius = %f m" % self.curv_R)
         
         self.simstart_x = 0
         self.simstart_y = 0
@@ -74,12 +80,14 @@ class SpeedVsCurvatureExperiment(object):
                 print("Velocity reached at (%f, %f) in %f s" % (x, y, self.simstart_time))
                 print("Circle params: cx = %f cy = %f R = %f" % (self.cx, self.cy, self.curv_R))
                 
-                curv_time = (pi*self.curv_R) / self.robot_vel # half circle
+                curv_time = (2*pi*self.curv_R) / self.robot_vel # half circle
                 self.simend_time = self.simstart_time + curv_time
-        elif abs(odom.twist.twist.linear.x - self.robot_vel) > 0.1 and self.time < self.simend_time + 0.5:
-            print("Warning: Robot is not following speed reference (speed_vs_curvature_systemID.py)")
+                print("times: %f %f" % (self.simstart_time, self.simend_time))
 
-        if self.simend_time != -1 and self.time < self.simend_time + 0.5:
+        # elif abs(odom.twist.twist.linear.x - self.robot_vel) > 0.1 and self.time < self.simend_time + 0.5:
+            # print("Warning: Robot is not following speed reference (speed_vs_curvature_systemID.py)")
+
+        if self.velocity_reached and self.time < self.simend_time + 0.5:
             pose_stamped = PoseStamped()
             pose_stamped.pose = odom.pose.pose
             self.robot_path.poses.append(pose_stamped)
@@ -98,6 +106,18 @@ class SpeedVsCurvatureExperiment(object):
             cx = x0 + (y0-cy)*tg0
         
         return cx, cy
+
+    def measure_radius(self):
+        x0 = self.simstart_x
+        y0 = self.simstart_y
+        max_dist = 0
+        for pose in self.robot_path.poses:
+            x = pose.pose.position.x
+            y = pose.pose.position.y
+            dist = sqrt((x-x0)**2 + (y-y0)**2)
+            if dist > max_dist:
+                max_dist = dist
+        return max_dist / 2
         
     def publish_paths(self):
         self.robot_path.header.frame_id = 'map'
@@ -108,8 +128,8 @@ class SpeedVsCurvatureExperiment(object):
         theta0 = atan2(self.simstart_y-self.cy, self.simstart_x-self.cx)
         for i in range(1000):
             pose_stamped = PoseStamped()
-            x = self.cx + self.curv_R*cos(theta0 - pi/1000.0*i) # direction
-            y = self.cy + self.curv_R*sin(theta0 - pi/1000.0*i) # direction
+            x = self.cx + self.curv_R*cos(theta0 - 2*pi/1000.0*i) # direction
+            y = self.cy + self.curv_R*sin(theta0 - 2*pi/1000.0*i) # direction
             pose_stamped.pose.position.x = x
             pose_stamped.pose.position.y = y
             trajectory.poses.append(pose_stamped)
@@ -119,13 +139,14 @@ class SpeedVsCurvatureExperiment(object):
 
 
     def run(self):
-        if not self.paths_published and self.simend_time != -1 and self.time > self.simend_time + 0.5:
+        if not self.paths_published and self.simend_time != -1 and self.time > self.simend_time:
             self.angle = 0
             self.robot_vel = 0
             self.simend_x = self.odom.pose.pose.position.x
             self.simend_y = self.odom.pose.pose.position.y
             self.publish_paths()
             self.paths_published = True
+            print("Measured curvature radius = %f m" % self.measure_radius())
             print("Simulation ended at %f" % self.time)
 
         acker = AckermannDriveStamped()
@@ -144,8 +165,8 @@ class SpeedVsCurvatureExperiment(object):
 
 if __name__ == '__main__':
     rospy.init_node('speed_vs_curvature_experiment')
-    experiment = SpeedVsCurvatureExperiment(10.0, 2.0)
-    rate = rospy.Rate(30) # 30 Hz
+    experiment = SpeedVsCurvatureExperiment(0.033, 6.8, 11.4)
+    rate = rospy.Rate(150) # 30 Hz
     rate.sleep()
     # real time calculation is dependent on publishing frequency
     while not rospy.is_shutdown():
